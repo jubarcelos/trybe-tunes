cf8faa95e7780e768265cc97b903694437beeec6
Julia Barcelos <julaai@gmail.com>
2021-11-05 18:55:25 -0300

    requisito 3 em andamento

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,34 +1,58 @@
-// import React from 'react';
+import React from 'react';
 
-// class Card extends React.Component {
-//   render() {
-//     const {
-//       cardName,
-//       cardDescription,
-//       cardAttr1,
-//       cardAttr2,
-//       cardAttr3,
-//       cardImage,
-//       cardRare,
-//       cardTrunfo,
-//     } = this.props;
-//     return (
-//       <div>
-//         <h1>Tryunfo</h1>
-//       </div>
-//     );
-//   }
-// }
+class Card extends React.Component {
+  render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+    } = this.props;
+    return (
+      <div>
+        <p data-testid="name-card">
+          { cardName }
+        </p>
 
-// export default Card;
+        <img
+          data-testid="image-card"
+          src={ cardImage }
+          alt={ cardName }
+        />
+        <p data-testid="description-card">
+          { cardDescription }
+        </p>
+        <p data-testid="attr1-card">
+          { cardAttr1 }
+        </p>
+        <p data-testid="attr2-card">
+          { cardAttr2 }
+        </p>
+        <p data-testid="attr3-card">
+          { cardAttr3 }
+        </p>
+        <p data-testid="rare-card">
+          { cardRare }
+        </p>
 
-// Form.propTypes = {
-//   cardName: PropTypes.string.isRequired,
-//   cardDescription: PropTypes.string.isRequired,
-//   cardAttr1: PropTypes.string.isRequired,
-//   cardAttr2: PropTypes.string.isRequired,
-//   cardAttr3: PropTypes.string.isRequired,
-//   cardImage: PropTypes.string.isRequired,
-//   cardRare: PropTypes.string.isRequired,
-//   cardTrunfo: PropTypes.bool.isRequired,
-// };
+      </div>
+    );
+  }
+}
+
+export default Card;
+
+Card.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.string.isRequired,
+  cardAttr2: PropTypes.string.isRequired,
+  cardAttr3: PropTypes.string.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+};
