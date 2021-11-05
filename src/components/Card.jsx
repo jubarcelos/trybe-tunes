5383950cef932629ecce49f7d8349538e8bad2d0
Julia Barcelos <julaai@gmail.com>
2021-11-05 17:59:55 -0300

    inicio do requisito 3 e comentada as funcoes que nao estavam sendo utilizadas.

---
+++ src/components/Card.jsx
@@ -0,0 +1,34 @@
+// import React from 'react';
+
+// class Card extends React.Component {
+//   render() {
+//     const {
+//       cardName,
+//       cardDescription,
+//       cardAttr1,
+//       cardAttr2,
+//       cardAttr3,
+//       cardImage,
+//       cardRare,
+//       cardTrunfo,
+//     } = this.props;
+//     return (
+//       <div>
+//         <h1>Tryunfo</h1>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Card;
+
+// Form.propTypes = {
+//   cardName: PropTypes.string.isRequired,
+//   cardDescription: PropTypes.string.isRequired,
+//   cardAttr1: PropTypes.string.isRequired,
+//   cardAttr2: PropTypes.string.isRequired,
+//   cardAttr3: PropTypes.string.isRequired,
+//   cardImage: PropTypes.string.isRequired,
+//   cardRare: PropTypes.string.isRequired,
+//   cardTrunfo: PropTypes.bool.isRequired,
+// };
