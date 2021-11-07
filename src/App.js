d7cc4942c4647aeb538a646133c5c78eeb6b9a6a
Julia Barcelos <julaai@gmail.com>
2021-11-07 16:07:39 -0300

    Requisito 5 finalizado. Todas as verificacoes de acordo.

--- src/App.js
+++ src/App.js
@@ -12,13 +12,11 @@ class App extends React.Component {
       cardAttr2: '',
       cardAttr3: '',
       cardImage: '',
-      cardRare: '',
+      cardRare: 'normal',
       cardTrunfo: false,
       hasTrunfo: false,
-      isSaveButtonDisabled: false,
+      isSaveButtonDisabled: true,
     };
-
-    // this.onInputChange = this.onInputChange.bind(this);
   }
   // função para definir que no checkbox não se pega o value, mas sim o checked. Sem um checkbox era só tirar o ternário.
   // como eu fiz arrow function, não preciso fazer o bind, já está declarado no this.
@@ -27,12 +25,43 @@ class App extends React.Component {
     const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
 
-    this.setState({
-      [name]: value,
+    this.setState({ [name]: value }, () => {
+      this.setState({ isSaveButtonDisabled: this.formValidation() });
     });
-  }
+  };
 
   onSaveButtonClick = () => { }
+
+  formValidation = () => {
+    const {
+      cardName,
+      cardDescription,
+      cardImage,
+    } = this.state;
+
+    let {
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+    } = this.state;
+
+    const maxSolo = 90;
+    const minSolo = 0;
+    const maxValueTotal = 210;
+    const atualState = Object.values(this.state);
+
+    cardAttr1 = Number(cardAttr1);
+    cardAttr2 = Number(cardAttr2);
+    cardAttr3 = Number(cardAttr3);
+    // tentei fazer a verificação de string vazia com um if || e não passava no test, Brunão me ajudou a ver que era esse meu problema.
+
+    if (atualState.some((verification) => verification === '')) return true;
+    if (cardAttr1 < minSolo || cardAttr1 > maxSolo || cardAttr1 === '') return true;
+    if (cardAttr2 < minSolo || cardAttr2 > maxSolo || cardAttr2 === '') return true;
+    if (cardAttr3 < minSolo || cardAttr3 > maxSolo || cardAttr3 === '') return true;
+    if ((cardAttr1 + cardAttr2 + cardAttr3) > maxValueTotal) return true;
+    return false;
+  }
 
   render() {
     const {
@@ -74,7 +103,6 @@ class App extends React.Component {
     );
   }
 }
+// no CR o Gabis deu a dica de armazenar os dados do state em uma variável para não reatribuir e o Guilherme Augusto me ajudou a entender melhor essa forma de fazer.
 
 export default App;
-
-// no CR o Gabis deu a dica de armazenar os dados do state em uma variável para não reatribuir e o Guilherme Augusto me ajudou a entender melhor essa forma de fazer.
