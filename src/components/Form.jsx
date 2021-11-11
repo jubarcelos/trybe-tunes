c3c10ab5e2590fdd52a29cad163e7941b708e31d
Julia Barcelos <julaai@gmail.com>
2021-11-11 13:42:13 -0300

    um pouco de ajuste para ajudar na estilizacao.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -21,82 +21,91 @@ class Form extends React.Component {
     return (
       <form action="">
         <fieldset>
-          <label htmlFor="cardName">
-            Nome:
-            <input
-              id="cardName"
-              name="cardName"
-              data-testid="name-input"
-              type="text"
-              value={ cardName }
-              onChange={ onInputChange }
-            />
-          </label>
-          <label
-            htmlFor="CardDescription"
-          >
-            Aqui algumas curiosidades:
-            <textarea
-              id="cardDescription"
-              name="cardDescription"
-              data-testid="description-input"
-              value={ cardDescription }
-              onChange={ onInputChange }
-            />
-          </label>
-
-          <label
-            htmlFor="CardAttr1"
-          >
-            Primeiro atributo:
-            <input
-              id="cardAttr1"
-              name="cardAttr1"
-              data-testid="attr1-input"
-              type="number"
-              value={ cardAttr1 }
-              onChange={ onInputChange }
-            />
-          </label>
-
-          <label
-            htmlFor="CardAttr2"
-          >
-            Segundo atributo:
-            <input
-              id="cardAttr2"
-              name="cardAttr2"
-              data-testid="attr2-input"
-              type="number"
-              value={ cardAttr2 }
-              onChange={ onInputChange }
-            />
-          </label>
-
-          <label
-            htmlFor="CardAttr3"
-          >
-            Terceiro atributo:
-            <input
-              id="cardAttr3"
-              name="cardAttr3"
-              data-testid="attr3-input"
-              type="number"
-              value={ cardAttr3 }
-              onChange={ onInputChange }
-            />
-          </label>
-          <label htmlFor="cardImage">
-            Insira a url com a imagem
-            <input
-              id="cardImage"
-              name="cardImage"
-              data-testid="image-input"
-              type="text"
-              value={ cardImage }
-              onChange={ onInputChange }
-            />
-          </label>
+          <div>
+            <label htmlFor="cardName">
+              Nome:
+              <input
+                id="cardName"
+                name="cardName"
+                data-testid="name-input"
+                type="text"
+                value={ cardName }
+                onChange={ onInputChange }
+              />
+            </label>
+          </div>
+          <div>
+            <label
+              htmlFor="CardDescription"
+            >Descrição:
+              <textarea
+                id="cardDescription"
+                name="cardDescription"
+                data-testid="description-input"
+                placeholder="Descreva esse card."
+                value={ cardDescription }
+                onChange={ onInputChange }
+              />
+            </label>
+          </div>
+          <div>
+            <label
+              htmlFor="CardAttr1"
+            >
+              Primeiro atributo:
+              <input
+                id="cardAttr1"
+                name="cardAttr1"
+                data-testid="attr1-input"
+                type="number"
+                value={ cardAttr1 }
+                onChange={ onInputChange }
+              />
+            </label>
+          </div>
+          <div>
+            <label
+              htmlFor="CardAttr2"
+            >
+              Segundo atributo:
+              <input
+                id="cardAttr2"
+                name="cardAttr2"
+                data-testid="attr2-input"
+                type="number"
+                value={ cardAttr2 }
+                onChange={ onInputChange }
+              />
+            </label>
+          </div>
+          <div>
+            <label
+              htmlFor="CardAttr3"
+            >
+              Terceiro atributo:
+              <input
+                id="cardAttr3"
+                name="cardAttr3"
+                data-testid="attr3-input"
+                type="number"
+                value={ cardAttr3 }
+                onChange={ onInputChange }
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="cardImage">
+              Insira a url com a imagem
+              <input
+                id="cardImage"
+                name="cardImage"
+                data-testid="image-input"
+                type="text"
+                value={ cardImage }
+                onChange={ onInputChange }
+              />
+            </label>
+          </div>
           <label htmlFor="cardRare">
             Raridade:
             <select
@@ -112,21 +121,23 @@ class Form extends React.Component {
             </select>
           </label>
 
-          {hasTrunfo
-            ? <span> Você já tem um Super Trunfo em seu baralho.</span>
-            : (
-              <label htmlFor="cardTrunfo">
-                É a super trunfo?
-                <input
-                  data-testid="trunfo-input"
-                  type="checkbox"
-                  id="cardTrunfo"
-                  name="cardTrunfo"
-                  checked={ cardTrunfo }
-                  onChange={ onInputChange }
-                />
-              </label>
-            )}
+          <div>
+            { hasTrunfo
+              ? <span> Você já tem um Super Trunfo em seu baralho.</span>
+              : (
+                <label htmlFor="cardTrunfo">
+                  É a super trunfo?
+                  <input
+                    data-testid="trunfo-input"
+                    type="checkbox"
+                    id="cardTrunfo"
+                    name="cardTrunfo"
+                    checked={ cardTrunfo }
+                    onChange={ onInputChange }
+                  />
+                </label>
+              ) }
+          </div>
 
           <button
             id="save"
